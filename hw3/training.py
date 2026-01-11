d9cf8e3fe0851a304e2d63b88bd7c74b668a632a
Yonatan Rappoport <79964176+yontigithub@users.noreply.github.com>
2026-01-11 20:17:16 +0200

    rnn

--- hw3/training.py
+++ hw3/training.py
@@ -97,12 +97,12 @@ class Trainer(abc.ABC):
             train_result = self.train_epoch(dl_train, verbose=verbose, **kw)
             epoch_len = len(train_result.losses)
 
-            train_loss.append(sum(train_result.losses)/epoch_len)
+            train_result = self.train_epoch(dl_train, verbose=verbose, **kw)
+            train_loss.append(sum(train_result.losses) / len(train_result.losses))
             train_acc.append(train_result.accuracy)
-
+    
             test_result = self.test_epoch(dl_test, verbose=verbose, **kw)
-
-            test_loss.append(sum(test_result.losses)/epoch_len)
+            test_loss.append(sum(test_result.losses) / len(test_result.losses))
             test_acc.append(test_result.accuracy)
 
             actual_num_epochs += 1
@@ -245,14 +245,14 @@ class RNNTrainer(Trainer):
     def train_epoch(self, dl_train: DataLoader, **kw):
         # TODO: Implement modifications to the base method, if needed.
         # ====== YOUR CODE: ======
-        pass
+        self.hidden_state = None
         # ========================
         return super().train_epoch(dl_train, **kw)
 
     def test_epoch(self, dl_test: DataLoader, **kw):
         # TODO: Implement modifications to the base method, if needed.
         # ====== YOUR CODE: ======
-        pass
+        self.hidden_state = None
         # ========================
         return super().test_epoch(dl_test, **kw)
 
@@ -270,7 +270,24 @@ class RNNTrainer(Trainer):
         #  - Update params
         #  - Calculate number of correct char predictions
         # ====== YOUR CODE: ======
-        pass
+        self.optimizer.zero_grad()
+        
+        logits, self.hidden_state = self.model(x)
+
+        if isinstance(self.hidden_state, torch.Tensor):
+            self.hidden_state = self.hidden_state.detach()
+        elif isinstance(self.hidden_state, tuple): # For LSTMs
+            self.hidden_state = tuple(s.detach() for s in self.hidden_state)
+        
+        logits_flat = logits.view(-1, logits.size(-1))
+        y_flat = y.view(-1)
+        
+        loss = self.loss_fn(logits_flat, y_flat)
+        loss.backward()
+        self.optimizer.step()
+        
+        hidden_states, predicted = torch.max(logits, dim=2)
+        num_correct = torch.sum(predicted == y)
         # ========================
 
         # Note: scaling num_correct by seq_len because each sample has seq_len
@@ -290,7 +307,14 @@ class RNNTrainer(Trainer):
             #  - Loss calculation
             #  - Calculate number of correct predictions
             # ====== YOUR CODE: ======
-            pass
+            logits, _ = self.model(x)
+            logits_flat = logits.view(-1, logits.size(-1))
+            y_flat = y.view(-1)
+            
+            loss = self.loss_fn(logits_flat, y_flat)
+            
+            hidden, predicted = torch.max(logits, dim=2)
+            num_correct = torch.sum(predicted == y)
             # ========================
 
         return BatchResult(loss.item(), num_correct.item() / seq_len)
